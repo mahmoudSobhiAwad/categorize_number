ec9dbd818cacc43d199955d38ef626f90d6527ef
modyelgen <modyelgen243@gmail.com>
2024-12-19 02:37:36 +0200

    add absolute to make sure of in switch case number is matching condition .

--- bin/categorize_numbers_list.dart
+++ bin/categorize_numbers_list.dart
@@ -4,7 +4,7 @@ void main(List<String> arguments) {
   // loop for each number in number list
   for (var number in numbersList) {
     //use switch case to check the remain of dividing process is 0 or 1 and default value in case any error happen.
-    switch (number % 2) {
+    switch (number.abs() % 2) {
       case 0:
         print("($number) is Even");
         break;
